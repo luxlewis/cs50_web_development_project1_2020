0ef653cc7522bc7b26f1b865f27fa0be50ca6c94
luxlewis <51483735+luxlewis@users.noreply.github.com>
2021-04-10 12:57:47 +0100

    Created entry page functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
 from . import util
+import markdown2
+
 
 
 def index(request):
@@ -8,3 +9,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    return render(request, "encyclopedia/title.html", {
+        "title": markdown2.markdown(util.get_entry(title))
+    })
