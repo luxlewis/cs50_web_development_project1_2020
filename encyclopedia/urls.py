c386edbf71b7e8f11a2d008b852204e2f76bfef7
luxlewis <51483735+luxlewis@users.noreply.github.com>
2021-04-11 17:20:00 +0100

    Added search bar to display entry page for query result

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.title, name="title")
+    path("search", views.search, name="search"),
+    path("<str:title>", views.title, name="title"),
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,22 +1,44 @@
 from django.shortcuts import render
 from . import util
 import markdown2
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django import forms
 
+
+class SearchForm(forms.Form):
+    title = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "search"
+    }))
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
-
-
     })
+
 
 def title(request, title):
     if util.get_entry(title):
         return render(request, "encyclopedia/title.html", {
-        "title": markdown2.markdown(util.get_entry(title))
-    })
+            "title": markdown2.markdown(util.get_entry(title))
+        })
     else:
         return render(request, 'encyclopedia/error.html', {
             "error": title
         })
+
+def search(request):
+    entries = util.list_entries()
+    query = request.GET.get("q")
+    if util.get_entry(query):
+        return render(request, "encyclopedia/search.html", {
+            "title": markdown2.markdown(util.get_entry(query))
+        })
+    else:
+        return render(request, 'encyclopedia/error.html', {
+            "error": query
+        })
+
+
+
