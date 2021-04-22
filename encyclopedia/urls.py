b4d1f66fdff2fc6ad992b2be1df7e6ab130a8f8e
luxlewis <51483735+luxlewis@users.noreply.github.com>
2021-04-22 12:40:37 +0100

    added edit entry page and a random entry page, so entries can be edited and saved, and clicking the random link will take user to a random entry page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new_page", views.new_page, name="new_page"),
+    path("wiki/random_page", views.random_page, name="random_page"),
+    path("wiki/new_page", views.new_page, name="new_page"),
     path("search_results", views.search, name="search_results"),
     path("search", views.search, name="search"),
     path("wiki/<title>", views.title, name="title"),
+    path("wiki/<title>/edit", views.edit_page, name="edit_page"),
 
 ]
